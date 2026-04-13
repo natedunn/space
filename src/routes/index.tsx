3b66f59f9a151bf2e96c71fc5d8a47718ee8a175
Nate Dunn <hello@natedunn.net>
2026-04-13 17:38:55 -0600

    update spacing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute("/")({ component: HomePage });
 
 function HomePage() {
 	return (
-		<div className="flex flex-col gap-24 overflow-x-clip">
+		<div className="flex flex-col gap-48 overflow-x-clip">
 			<header className="mx-auto flex max-w-5xl flex-col gap-8 px-12 pt-24 md:pt-48">
 				<h1 className="text-3xl sm:text-4xl md:text-7xl font-semibold">
 					nate is <span className="text-muted-foreground">making things</span>{" "}
